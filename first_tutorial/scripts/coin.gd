1df27d6374520a7bed22ae5653a02a452cb56874
memi394 <emre203m@gmail.com>
2025-10-29 01:26:17 +0300

    Add files via upload

---
+++ first_tutorial/scripts/coin.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+@onready var game_manager: Node = %gameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _on_body_entered(_body):
+	game_manager.add_point()
+	animation_player.play("pick_up")
